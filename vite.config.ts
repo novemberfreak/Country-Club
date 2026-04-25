a862a6ffc93b07fc0015a83433973890a86add87
novemberfreak <mitchellamegbe12@gmail.com>
2026-04-25 02:52:43 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react(), tailwindcss()],
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Country-Club/',
+  plugins: [react()],
+}))],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
